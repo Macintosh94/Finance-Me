8861643cbc1901f3e7c6f6ef53157535da05e4e3
Palash <parpanipalash@gmail.com>
2023-06-03 15:37:44 +0530

    Test Terraform Configuration

--- Test-Server_Infra.tf
+++ Test-Server_Infra.tf
@@ -1,86 +1,34 @@
-terraform{
-         required_providers {
-                 aws = {
-                         source = "hashicorp/aws"
-                         version = "~> 4.0"
-                 }
-         }
+locals {
+  vpc_id           = "vpc-09e7ff8aeb1ef8610"
+  subnet_id        = "subnet-0325f173ed2ce59ad"
+  ssh_user         = "ubuntu"
+  key_name         = "AWS_Key"
+  private_key_path = "/home/ubuntu/AWS_Key.pem"
 }
-
-# Configure Providers
 
 provider "aws" {
-  region  = "us-east-1"
-  access_key = "AKIATVOIMA34MRX6I4EK"
-  secret_key = "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"
-
+  region = "us-east-1"
 }
 
-# Deploy Ec2 instances
+resource "aws_instance" "test-server" {
+  ami                         = "ami-053b0d53c279acc90"
+  subnet_id                   = "subnet-0325f173ed2ce59ad"
+  instance_type               = "t2.micro"
+  associate_public_ip_address = true
+  security_groups             = "sg-0230fea44fc45815f"
+  key_name                    = local.key_name
 
-resource "aws_instance" "test-server"{
-        ami = "ami-053b0d53c279acc90"
-        key_name = "AWS_Key"
-        instance_type = "t2.micro"
-        tags = {
-        Name = "Test-Server"
-        }
+  provisioner "remote-exec" {
+    inline = ["echo 'Wait until SSH is ready'"]
+
+    connection {
+      type        = "ssh"
+      user        = local.ssh_user
+      private_key = file(local.private_key_path)
+      host        = aws_instance.test-server.public_ip
+    }
+  }
+  provisioner "local-exec" {
+    command = "ansible-playbook  -i ${aws_instance.test-server.public_ip}, --private-key ${local.private_key_path} test-deployment.yaml"
+  }
 }
-
-resource "null_resource" "localinventorynull01" {
-         triggers = {
-                 mytest = timestamp()
-         }
-        
-         provisioner "local-exec" {
-             command = "echo ${aws_instance.test-server.tags.Name} ansible_host=${aws_instance.test-server.public_ip} ansible_user=ec2-user>> inventory"
-
-           }
-
- 
-         depends_on = [
-
-                          aws_instance.test-server
-                          ]
-          }
-
-
-resource "null_resource" "mydynamicinventory" {
-
-        triggers = {
-                mytest = timestamp()
-        }
-
-        provisioner "local-exec" {
-            command = "scp inventory ubuntu@54.89.174.78:/tmp/"
-  
-          }
-        depends_on = [
-                       
-                        null_resource.localinventorynull02 , null_resource.localinventorynull01
-                        ]
-}
-
-resource  "null_resource"  "ssh3" {
-
-	triggers = {
-		mytest = timestamp()
-	}
-
-	connection {
-	    type     = "ssh"
-	    user     = "ec2-user"
-	    private_key = "/home/ubuntu/AWS_Key.pem"
-	    host     = aws_instance.test-server
-	  }
-
-	provisioner "remote-exec" {
-	    inline = [
-              "sudo chmod 777 /tmp/inventory",
-              "sudo mv /tmp/inventory /root/ansible/inventory",
-	    ]
-	  }
-}
-
-
-
