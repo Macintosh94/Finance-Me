8b8573645d54219b809397ddccf5e1847faaf138
Palash <parpanipalash@gmail.com>
2023-06-03 20:28:15 +0530

    .

--- Test-Server_Infra.tf
+++ Test-Server_Infra.tf
@@ -69,6 +69,50 @@ resource "aws_route_table_association" "test-rt-sub-association" {
   route_table_id = aws_route_table.test-rt.id
 }
 
+resource "aws_security_group" "test-sg" {
+    name = "test-sg"
+    description = "Enable ports 22, 80, 443"  
+    vpc_id = aws_vpc.test-vpc.id
+
+    ingress {
+
+        description = "HTTPS Traffic"
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = "::/0"
+    }
+    ingress {
+
+        description = "HTTP Traffic"
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+    
+    ingress {
+
+        description = "SSH Port"
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+
+    egress {
+
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+        ipv6_cidr_blocks = ["::/0"]
+    }
+}
+
 resource "aws_network_interface" "test-ni" {
    subnet_id  = aws_subnet.test-subnet.id
    private_ips = ["10.0.11.77"]
