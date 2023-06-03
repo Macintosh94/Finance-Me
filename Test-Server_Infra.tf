b03eae4ffcfc0ea071f34d30311cc7aaaf583a1b
Palash <parpanipalash@gmail.com>
2023-06-03 19:20:35 +0530

    .

--- Test-Server_Infra.tf
+++ Test-Server_Infra.tf
@@ -58,7 +58,7 @@ resource "aws_subnet" "test-subnet" {
    }
 }
 
-resource "aws_route_table_association" "test-rt-sub.association" {
+resource "aws_route_table_association" "test-rt-sub-association" {
   subnet_id = aws_subnet.test-subnet.id
   route_table_id = aws_route_table.test-rt.id
 }
